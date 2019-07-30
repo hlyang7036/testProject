b5683256f7720c475704d1617bd6d93f9b153589
ha <hlyang7036@gmail.com>
2019-07-30 09:59:34 +0900

    firstCommit

---
+++ src/main/java/com/bit/JpaEx/SpringBootJapEx2Application.java
@@ -0,0 +1,13 @@
+package com.bit.JpaEx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJapEx2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJapEx2Application.class, args);
+	}
+
+}
